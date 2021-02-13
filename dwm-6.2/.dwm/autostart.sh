60c9fb52768bf4e24054ec78b934f6bdf9611b14
Didiloy <78911744+Didiloy@users.noreply.github.com>
2021-02-13 14:50:50 +0100

    Update autostart.sh

--- dwm-6.2/.dwm/autostart.sh
+++ dwm-6.2/.dwm/autostart.sh
@@ -3,3 +3,4 @@ xrandr --output HDMI-1 --auto --left-of eDP-1
 compton -o 0.80 &
 feh --bg-scale /home/dylan/Téléchargements/fond_ecran/meuf_cafe_voiture.jpg &
 qutebrowser &
+/home/dylan/dwm-bar/dwm_bar.sh &
