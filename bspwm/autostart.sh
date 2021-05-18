82315f6f24df058e5f910ab9fd73962f01349f32
Didiloy <78911744+Didiloy@users.noreply.github.com>
2021-05-18 22:34:43 +0200

    Add files via upload

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -2,8 +2,8 @@
 #xrandr --output HDMI-1 --auto --left-of eDP-1
 xrandr --output HDMI-1-1 --auto --left-of eDP-1-1
 xinput --set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled" 1
+feh --bg-scale /home/dylan/Téléchargements/fond_ecran/meuf_cafe_voiture.jpg &
 killall compton
 compton
-feh --bg-scale /home/dylan/Téléchargements/fond_ecran/meuf_cafe_voiture.jpg &
-mount /dev/sda1 /media/dylan/DATA/
+eww-daemon
 qutebrowser &
