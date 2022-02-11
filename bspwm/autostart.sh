cfeac9fa9df782f95b4ca62416b22a52da36c98c
Didiloy <didiloy2001@gmail.com>
2022-02-11 15:33:07 +0100

    ajout brave dans sxhkdrc et bspwmrc

--- bspwm/autostart.sh
+++ bspwm/autostart.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#xrandr --output HDMI-1 --auto --left-of eDP-1
-xrandr --output HDMI-1-1 --auto --left-of eDP-1-1
+xrandr --output HDMI-1 --auto --left-of eDP-1
+#xrandr --output HDMI-1-1 --auto --left-of eDP-1-1
 xinput --set-prop "ETPS/2 Elantech Touchpad" "libinput Natural Scrolling Enabled" 1
-feh --bg-scale /home/dylan/Téléchargements/fond_ecran/meuf_cafe_voiture.jpg &
+feh --bg-scale /home/dylan/Téléchargements/fond_ecran/mer_sainte_anne.jpg &
 killall compton
 compton
-eww-daemon
-qutebrowser &
+brave-browser &
